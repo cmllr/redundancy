283bca022ea64f77590bdb1b2130b7b0a673f61f
sqarerootfury <fury224@googlemail.com>
2013-05-06 19:47:08 +0300

    Update DataBase.inc.php

--- Includes/DataBase.inc.php
+++ Includes/DataBase.inc.php
@@ -1,5 +1,5 @@
 <?php
 	//DataBase Connection file.
-	$connect = mysql_connect("localhost", "root", "90104e58") or die("DataBase Error: '".mysql_error());
+	$connect = mysql_connect("localhost", "user", "pass") or die("DataBase Error: '".mysql_error());
 	mysql_select_db("Scambio",$connect) or die("");
 ?>
