d2d5fbec11ee6c42b27e37bb5266c3f6d778a201
sqarerootfury <fury224@googlemail.com>
2013-07-30 17:50:46 +0200

    Update Redundancy.sql
    Replaced the SQL - dump with the latest version

--- Documents/Redundancy.sql
+++ Documents/Redundancy.sql
@@ -1,12 +1,3 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 CREATE TABLE IF NOT EXISTS `Banned` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `IP` text,
@@ -14,8 +5,7 @@ CREATE TABLE IF NOT EXISTS `Banned` (
   `Date` text,
   `Reason` text NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 CREATE TABLE IF NOT EXISTS `Files` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Filename` text CHARACTER SET utf8 COLLATE utf8_bin,
@@ -31,33 +21,31 @@ CREATE TABLE IF NOT EXISTS `Files` (
   `Client` text,
   `ReadOnly` tinyint(1) NOT NULL,
   `MimeType` text,
+  `Bin` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1252 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 CREATE TABLE IF NOT EXISTS `Mails` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Text` text NOT NULL,
   `Description` text NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-INSERT INTO `Mails` (`ID`, `Text`, `Description`) VALUES
-(1, 'Hallo %s,\n\ndu hast dich bei %s registriert. Um deinen Account zu aktivieren, klicke bitte folgenden Link an:\n\n%s\n\nDu hast diese Email erhalten, weil du dich bei %s registriert hast.', 'Register email in german language'),
-(2, 'Hallo %s,\n\ndein Passwort %s wurde zurückgesetzt. Das Passwort wurde auf\n\n%s\n\ngesetzt. Du bekommst diese Mail, weil du bei %s registriert bist.\n', 'New Password Description de'),
-(3, 'Hallo %s,\n\ndein Account wurde gesperrt, es wurde zu oft versucht sich mit deinen Logindaten anzumelden.\n\nDie IP lautete: %s.\n\n%s%s\n', 'Account_Locked');
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 CREATE TABLE IF NOT EXISTS `Pass_History` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Changed` text COLLATE utf8_bin,
   `IP` text COLLATE utf8_bin NOT NULL,
   `Who` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;##
 CREATE TABLE IF NOT EXISTS `Share` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Hash` text NOT NULL,
   `UserID` int(11) NOT NULL,
   `Extern_ID` text NOT NULL,
   `Used` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID` (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 CREATE TABLE IF NOT EXISTS `Users` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `User` text,
@@ -73,8 +61,6 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `API_Key` text NOT NULL,
   `Enable_API` int(11) DEFAULT NULL,
   `Failed_Logins` int(11) NOT NULL DEFAULT '0',
+  `Session_Closed` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
