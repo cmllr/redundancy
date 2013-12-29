1afe8a76e52898334819c90917fa868447d619bf
squarerootfury <fury224@googlemail.com>
2013-12-29 13:06:36 +0100

    Update program release date

--- Includes/Program.inc.php
+++ Includes/Program.inc.php
@@ -21,7 +21,7 @@
 	 * Program entry point
 	 */
 	$GLOBALS["Program_Version"] = "1.9.13-git-beta3-1";
-	$GLOBALS["Program_Release"] = "27.12.2013";
+	$GLOBALS["Program_Release"] = "29.12.2013";
 	$GLOBALS["Program_Codename"] = "Cumulus";
 	$GLOBALS["config_dir"] = "./";	
 	Include "Kernel/Kernel.User.inc.php";
