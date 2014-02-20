77178e71d684d3733decc43dfa06ec2b47f70dbf
squarerootfury <fury224@googlemail.com>
2014-02-20 10:10:54 +0100

    Updated installer database dump

--- Installer/database.sql
+++ Installer/database.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `Banned` (
   `Date` text,
   `Reason` text NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;##
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;##
 
 CREATE TABLE IF NOT EXISTS `Files` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -27,13 +27,21 @@ CREATE TABLE IF NOT EXISTS `Files` (
   `lastWrite` datetime NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;##
-
+CREATE TABLE IF NOT EXISTS `LocalShare` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `FileID` int(11) NOT NULL,
+  `TargetUser` int(11) NOT NULL,
+  `Mode` int(11) NOT NULL COMMENT 'See CHMOD',
+  PRIMARY KEY (`ID`),
+  KEY `FileID` (`FileID`),
+  KEY `TargetUser` (`TargetUser`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;##
 CREATE TABLE IF NOT EXISTS `Mails` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Text` text NOT NULL,
   `Description` text NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;##
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;##
 
 CREATE TABLE IF NOT EXISTS `Pass_History` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -41,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `Pass_History` (
   `IP` text COLLATE utf8_bin NOT NULL,
   `Who` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;##
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;##
 
 CREATE TABLE IF NOT EXISTS `Settings` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -52,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `Settings` (
   `Program_Enable_Preview` int(11) NOT NULL,
   `Program_Enable_KeyHooks` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;##
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;##
 
 CREATE TABLE IF NOT EXISTS `Share` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -63,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `Share` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID` (`ID`),
   KEY `ID_2` (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;##
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;##
 
 CREATE TABLE IF NOT EXISTS `Users` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -80,16 +88,8 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `Failed_Logins` int(11) NOT NULL DEFAULT '0',
   `Session_Closed` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;##
-CREATE TABLE IF NOT EXISTS `LocalShare` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `FileID` int(11) NOT NULL,
-  `TargetUser` int(11) NOT NULL,
-  `Mode` int(11) NOT NULL COMMENT 'See CHMOD',
-  PRIMARY KEY (`ID`),
-  KEY `FileID` (`FileID`),
-  KEY `TargetUser` (`TargetUser`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;##
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;##
+
 ALTER TABLE `LocalShare`
   ADD CONSTRAINT `LocalShare_ibfk_1` FOREIGN KEY (`FileID`) REFERENCES `Files` (`ID`),
   ADD CONSTRAINT `LocalShare_ibfk_2` FOREIGN KEY (`TargetUser`) REFERENCES `Users` (`ID`);
