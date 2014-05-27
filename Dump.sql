acc1a2ffc2f70c4e6cb31dd7bda878d54e86f5e7
squarerootfury <fury224@googlemail.com>
2014-05-27 19:06:09 +0200

    Added missing values on the database dump

--- Dump.sql
+++ Dump.sql
@@ -1,6 +1,29 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE TABLE IF NOT EXISTS `Role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` text COLLATE utf8_bin,
+  `permissions` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+
+INSERT INTO `Role` (`id`, `description`, `permissions`) VALUES
+(1, 'Root', 1);
+
+CREATE TABLE IF NOT EXISTS `Session` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL,
+  `token` text COLLATE utf8_bin NOT NULL,
+  `sessionStartedDateTime` datetime NOT NULL,
+  `sessionEndDateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userID` (`userID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=33 ;
+
+INSERT INTO `Session` (`id`, `userID`, `token`, `sessionStartedDateTime`, `sessionEndDateTime`) VALUES
+(32, 9, '3c4ea6c0d772988ba779b465ab515a2b', '2014-05-18 15:38:27', '0000-00-00 00:00:00');
+
 CREATE TABLE IF NOT EXISTS `User` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `loginName` text COLLATE utf8_bin,
@@ -22,5 +45,5 @@ INSERT INTO `User` (`id`, `loginName`, `displayName`, `mailAddress`, `registrati
 (9, 'fuxry', 'CM', 'bla@blxa.de', '2014-05-17 14:12:59', '0000-00-00 00:00:00', '$2y$11$KY/Tjuko2xX/4WqhhyYj6.FnzdN/9Ui7D2JUUujy/bPSVKheUoKJO', 1, 5242880, 1, 0);
 
 
-ALTER TABLE `User`
-  ADD CONSTRAINT `fk_Role` FOREIGN KEY (`roleID`) REFERENCES `Role` (`id`);
+ALTER TABLE `Session`
+  ADD CONSTRAINT `Session_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `User` (`id`);
