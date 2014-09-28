634c034cdc6328db805d279ecd29da604ff34d75
squarerootfury <fury224@googlemail.com>
2014-09-28 18:52:42 +0200

    Lib cleanup (cont)

--- index.php
+++ index.php
@@ -9,21 +9,21 @@ ob_start();
 ?>
 <!DOCTYPE html>
 <head>
-<meta charset="UTF-8">	
+<meta charset="UTF-8">
+<!-- jQuery -->
+<script src="./Lib/jQuery/jquery-1.10.2.min.js"></script>	
 <!-- Bootstrap -->
 <link rel = 'stylesheet' href='./Lib/Lenticularis/r2-bootstrap.css' type = 'text/css'/>
 <script src='./Lib/Bootstrap/js/bootstrap.min.js'></script>
 <!-- FontAwesome -->
-<link rel="stylesheet" href="./Lib/FontAwesome/font-awesome.css">
-<!-- jQuery -->
-<script src="./Lib/jQuery/jquery-1.10.2.min.js"></script>
+<link rel="stylesheet" href="./Lib/FontAwesome/css/font-awesome.css">
 <!-- jQuery UI -->
 <script src="./Lib/jQuery/UI/jquery-ui.js"></script>
 <!-- MISSING -->
 <!-- jQuery Context Menu -->
-<script src='./Lib/jquery.contextMenu.js'></script>
-<script src='./Lib/jquery.ui.position.js'></script>
-<link rel="stylesheet" href="./Lib/jquery.contextMenu.css">
+<script src='./Lib/jQuery/jquery.contextMenu.js'></script>
+<script src='./Lib/jQuery/jquery.ui.position.js'></script>
+<link rel="stylesheet" href="./Lib/jQuery/jquery.contextMenu.css">
 <!-- Spin.js -->
 <script src='./Lib/spin.min.js'></script>
 <!-- Dropzone.js -->
