4c615b8842c5c514c07c99d6134161afb0528198
squarerootfury <fury224@googlemail.com>
2014-10-11 19:53:30 +0200

    Typo fix 2

--- Includes/Kernel/Kernel.Installer.class.php
+++ Includes/Kernel/Kernel.Installer.class.php
@@ -139,7 +139,7 @@
 			return password_hash($password, PASSWORD_BCRYPT, $options);
 		}
 		public function SetUser($user,$pass){
-			require_once __REDUNDANCY_ROOT__."Includes/Kernel/Kernel.DBLayer.class.php";
+			require_once __REDUNDANCY_ROOT__."Includes/Kernel/Kernel.Config.class.php";
 			try{
 				$classLoader = new ClassLoader('Doctrine\DBAL', __REDUNDANCY_ROOT__."Lib/Doctrine");
 				$commonLoader = new ClassLoader('Doctrine\Common', __REDUNDANCY_ROOT__."Lib/Doctrine");
