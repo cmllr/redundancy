d8620f77256b1c3b895e3bf8f5a2a314601eaaa0
squarerootfury <fury224@googlemail.com>
2015-01-23 14:55:59 +0100

    Fixed #88 (experimental commit)

--- nys/Views/Files.php
+++ nys/Views/Files.php
@@ -278,5 +278,62 @@ nys.Init();
 		}
 	});  	
   } 
+   //Upload drop
+	$('#list').on(
+	    'dragover',
+	    function(e) {
+	        e.preventDefault();
+	        e.stopPropagation();
+	    }
+	)
+	$('#list').on(
+	    'dragenter',
+	    function(e) {
+	    	$("#uploadbox").attr("style","");
+	    	$("#uploadbox").dialog({title:"<?php echo $GLOBALS['Language']->Upload_Title.'</span> '.$_SESSION['currentFolder'];?>",onClose:function(){		    			
+				nys.Init();
+			}});
+	        e.preventDefault();
+	        e.stopPropagation();
+	    }
+	)	 
+
 </script>
 </table>
+<div id="uploadbox" style="visibility:hidden;height:0px">
+<div id = 'result'></div>
+
+<form class ='dropzone' id = 'my-awesome-dropzone' action='?upload' method='POST' >
+<div class = 'dz-message'>
+    <h3 class="text-center"><span class='fa fa-file'>&nbsp;</span>
+       <?php echo $GLOBALS['Language']->dictUploadTitle ;?>
+    </h3>
+</div>
+	<div class='fallback'>
+    <input name='file' type='file'/>
+  </div>
+</form>
+
+<script>
+Dropzone.options.myAwesomeDropzone = {
+  paramName: 'file', 
+  uploadMultiple: true,
+  addRemoveLinks: true,
+  parallelUploads: 1,
+  accept: function(file, done) {
+   done();
+  
+  },
+  complete: function(){ 
+  	nys.Init();
+  },
+ // error: function(e){
+  //  console.log(e);
+  //},
+  dictRemoveFile: '<?php echo $GLOBALS['Language']->dictRemoveFile;?>',
+  dictCancelUpload: '<?php  echo $GLOBALS['Language']->dictCancelUpload;?>',
+  dictDefaultMessage: '<?php  echo $GLOBALS['Language']->dictDefaultMessage;?>',  
+  dictCancelUploadConfirmation: '<?php  echo $GLOBALS['Language']->dictCancelUploadConfirmation;?>',  
+};
+</script>
+</div>
