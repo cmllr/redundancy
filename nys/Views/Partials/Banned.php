177d40077987fde4ae19e8a740936b9ac146fdcd
squarerootfury <fury224@googlemail.com>
2015-05-16 15:16:44 +0200

    Snapshot 16.05.2015 15:16 by <xf8@xf8> [r2-Buildbot]

--- nys/Views/Partials/Banned.php
+++
@@ -1,5 +0,0 @@
-<h2>Banned</h2>
-<div class="well">
-<i class="fa fa-ban"></i> Your IP <?php echo $ip;?> has been blocked from this Redundancy Server. Contact the system administrator for unblocking.
-</div>
-<img class="branding" src="./nys/Views/img/logoWithTextSmall.png">
