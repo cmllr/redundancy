40615dfa03b1ac838b644fe424b3bc03ade47a85
squarerootfury <fury224@googlemail.com>
2016-10-03 00:01:25 +0200

    + Bootstrap downgrade

--- arcus/index.php
+++ arcus/index.php
@@ -1,7 +1,7 @@
 <html>
   <head>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.4/css/bootstrap.min.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/css/tether-theme-basic.min.css" />
      <link rel="stylesheet" href="./arcus.css">     
@@ -18,7 +18,7 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.slim.min.js"></script>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.4/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
 
 <script>
@@ -53,7 +53,9 @@ const app = new Vue({
 
   },
   computed: {
-    
+    instance:function(){
+      return window.location.hostname
+    }
   },
   methods: {
     
--- arcus/routes/LogIn.vue
+++ arcus/routes/LogIn.vue
@@ -1,4 +1,4 @@
-<script type="text/x-template" id="login">
+<script type="text/x-template" id="login">    
     <div class="row">
         <div class="col-lg-4">
         </div>
